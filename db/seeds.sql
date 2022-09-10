2c3442e7df1739c7cf38142205ed6a8712edbd91
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-09-10 13:27:51 -0400

    updated seeds.sql with correct number of columns

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,27 @@
-INSERT INTO department (id, name)
+INSERT INTO department (name)
 VALUES ("Sales"),
        ("Engineering"),
        ("Finance"),
        ("Legal" );
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES ("Sales Lead", 100000),
-       ("Salesperson", 80000),
-       ("Lead Engineer", 150000),
-       ("Software Engineer", 120000),
-       ("Account Manager", 160000),
-       ("Accountant", 125000),
-       ("Legal Team Lead", 250000),
-       ("Lawyer", 190000);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1),
+       ("Salesperson", 80000, 1),
+       ("Lead Engineer", 150000, 2),
+       ("Software Engineer", 120000, 2),
+       ("Account Manager", 160000, 3),
+       ("Accountant", 125000, 3),
+       ("Legal Team Lead", 250000, 4),
+       ("Lawyer", 190000, 4);
     
        
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe"),
-       ("Mike", "Chan"),
-       ("Ashley", "Rodriguez"),
-       ("Kevin", "Tupik"),
-       ("Kunal", "Singh"),
-       ("Malia", "Brown"),
-       ("Sarah", "Lourd"),
-       ("Tom", "Allen");
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, NULL),
+       ("Mike", "Chan", 1, NULL),
+       ("Ashley", "Rodriguez", 1, NULL),
+       ("Kevin", "Tupik", 1, NULL),
+       ("Kunal", "Singh", 1, NULL),
+       ("Malia", "Brown", 1, NULL),
+       ("Sarah", "Lourd", 1, NULL),
+       ("Tom", "Allen", 1, NULL);
 
