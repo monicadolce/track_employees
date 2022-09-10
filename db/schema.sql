9e5697c305ca60912eab7612e89af3662333401e
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-09-10 10:32:11 -0400

    fixed schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -14,9 +14,8 @@ id INT NOT NULL AUTO_INCREMENT,
 title VARCHAR(30),
 salary DECIMAL,
 department_id INT,
-PRIMARY KEY (id)
-FOREIGN KEY (department_id)
-REFERENCES department(id)
+PRIMARY KEY (id),
+FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -25,9 +24,7 @@ first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INT,
 manager_id INT,
-PRIMARY KEY (id)
-FOREIGN KEY (role_id)
-REFERENCES role(id)
-FOREIGN KEY (manager_id)
-REFERENCES employee(id)
+PRIMARY KEY (id),
+FOREIGN KEY (role_id) REFERENCES role(id),
+FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
